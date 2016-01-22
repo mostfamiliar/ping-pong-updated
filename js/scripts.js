7f265996a6ee0cb97807b73a0206c27abb89ab23
Torrence Stratton <tlstratton@gmail.com>
2016-01-22 09:43:22 -0800

    Add array for processing

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
 var pingPonger = function(number) {
   var num = parseInt(number);
+  var result =[];
 
   for (var i = 1; i <= num; i++) {
     if (i % 5 === 0 && i % 3 === 0) {
-      var result = "ping pong";
-    }
-    else if (i % 3 === 0) {
-      var result = "ping";
-      }
-    else if (i % 5 === 0) {
-      var result = "pong";
-    }
-    else {
+      result.push("pingpong");
 
     }
+    else if (i % 3 === 0) {
+      result.push("ping");
+      }
+    else if (i % 5 === 0) {
+      result.push("pong");
+    }
+    else {
+      result.push(i);
+    }
+    var processedOutput = result.join(" ");
   };
-  return result;
+   return processedOutput;
 };
 
 $(document).ready(function() {
   $("form#numeralProcess").submit(function(event) {
-    var num = $("#numberInput").val();
-    var output = pingPonger(num);
-    $("#textOutput").text(output);
+    var number = $("input#numberInput").val();
+    console.log(number);
+    var output = pingPonger(number);
+    $("#textOutput").text(output + "<br>");
     (event).preventDefault();
   })
 });
