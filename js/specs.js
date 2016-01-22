31b998db71a9ea9cc4b9f9e266c132b5fa36ec19
Torrence Stratton <tlstratton@gmail.com>
2016-01-22 08:10:41 -0800

    Add initial site structure

---
+++ js/specs.js
@@ -0,0 +1,11 @@
+describe("pingPong", function() {
+  it("will replace numbers divisible by 3 with ping", function() {
+    expect(pingpong(6)).to.equal("ping")
+  });
+  it("will replace numbers divisible by 5 with pong", function() {
+    expect(pingpong(10)).to.equal("pong")
+  }
+  it("will replace numbers divisible by 3 and 15 with ping pong" function() {
+    expect(pingpong(30)).to.equal("ping pong")
+  });
+}
