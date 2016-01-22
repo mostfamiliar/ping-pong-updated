1dcb1b26cec66106316ee529a11c161cf3de5997
Torrence Stratton <tlstratton@gmail.com>
2016-01-22 08:13:00 -0800

    Setup BDD specs

--- js/specs.js
+++ js/specs.js
@@ -8,4 +8,7 @@ describe("pingPong", function() {
   it("will replace numbers divisible by 3 and 15 with ping pong" function() {
     expect(pingpong(30)).to.equal("ping pong")
   });
+  it("will produce new results when a new number is entered" function() {
+    expect(pingpong(15, 5)).to.equal("ping pong, pong")
+  });
 }
