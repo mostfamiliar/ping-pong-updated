26249f55375db3e6687f1ed703e462a3e0d40bec
Torrence Stratton <tlstratton@gmail.com>
2016-01-22 16:33:24 -0800

    add H1 bounce

--- js/scripts.js
+++ js/scripts.js
@@ -33,24 +33,28 @@ var pingPonger = function(number) {
 
 
 $(document).ready(function() {
+  $("h1").mouseover(function() {
+  $("h1").effect("bounce", {distance: 10, times: 1},"slow")
+  });
+
   $("form#processNumber").submit(function(event) {
     var number = $("input#numberInput").val();
     var validation = pingpongValidator(number);
 
     if (!validation) {
       $(".form-group").attr("class", "form-group has-error has-feedback");
-      (event).preventDefault();
-    }
-    else {
+      event.preventDefault();
+
+    } else {
       var output = pingPonger(number);
       $("#textOutput").empty();
       // $("#textOutput").empty().append("<li>" + output.join("</li><li>"));
       for (var x = 0; x < output.length; x++) {
         looper = output[x];
         var $test = $("<li>").text(looper).hide().delay(100*x).fadeIn(300);
-        $('#textOutput').append($test);
+        $("#textOutput").append($test);
       }
-      (event).preventDefault();
+    event.preventDefault();
     }
 
   });
